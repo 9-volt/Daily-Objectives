fc9cc9581ee58890cedcfb6c612883c5ee786816
DanD21 <dan.danilescu@gmail.com>
2015-07-23 15:17:56 +0300

    6. Added a detail view page with all information. Visual tweaks: colored navigation bar, white status bar, white navigation buttons, fonts. Separators not visible.

---
+++ Daily-Objective/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  Daily-Objective
+//
+//  Created by Dan on 23.07.15.
+//  Copyright (c) 2015 9volt. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
